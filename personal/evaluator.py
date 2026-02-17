ef04695ecbe5481549598ad746f3b09820755b88
Jacob Drahuschak Jr <151879582+Hiefin@users.noreply.github.com>
2026-02-16 20:30:43 -0500

    HW1 personal folder added

---
+++ personal/evaluator.py
@@ -0,0 +1,59 @@
+import parser, tokenizer
+
+
+def evaluate(ast):
+    if ast["tag"] == "number":
+        return ast["value"]
+    elif ast["tag"] == "+":
+        return evaluate(ast["left"]) + evaluate(ast["right"])
+    elif ast["tag"] == "-":
+        return evaluate(ast["left"]) - evaluate(ast["right"])
+    elif ast["tag"] == "*":
+        return evaluate(ast["left"]) * evaluate(ast["right"])
+    elif ast["tag"] == "/":
+        return evaluate(ast["left"]) / evaluate(ast["right"])
+    elif ast["tag"] == "%":
+        right = evaluate(ast["right"])
+        if right == 0:
+            raise ValueError(f"Cant use Modulo with a zero")
+        return evaluate(ast["left"]) % right
+    else:
+        raise ValueError(f"Unknown AST node: {ast}")
+
+
+def test_evaluate():
+    print("test evaluate()")
+    ast = {"tag": "number", "value": 3}
+    assert evaluate(ast) == 3
+    ast = {
+        "tag": "+",
+        "left": {"tag": "number", "value": 3},
+        "right": {"tag": "number", "value": 4},
+    }
+    assert evaluate(ast) == 7
+    ast = {
+        "tag": "*",
+        "left": {
+            "tag": "+",
+            "left": {"tag": "number", "value": 3},
+            "right": {"tag": "number", "value": 4},
+        },
+        "right": {"tag": "number", "value": 5},
+    }
+    assert evaluate(ast) == 35
+    tokens = tokenizer.tokenize("3*(4+5)")
+    ast, tokens = parser.parse_expression(tokens)
+    assert evaluate(ast) == 27
+    ast = {"tag": "%",
+           "left": {"tag": "number", "value": 10},
+           "right": {"tag": "number", "value": 8},
+           }
+    assert evaluate(ast) == 2
+    tokens = tokenizer.tokenize("10+12%5")
+    ast, tokens = parser.parse_expression(tokens)
+    assert evaluate(ast) == 12
+
+
+if __name__ == "__main__":
+    test_evaluate()
+    print("done.")
